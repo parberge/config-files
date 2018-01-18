d57857d2eb26c68df31cb0c769e2368d48e86720
Pär Berge <parberge@MacBook-Pro.peers.se>
2018-01-18 21:25:23 +0100

    Add force option to setup_configs.sh

--- setup_configs.sh
+++ setup_configs.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
+force=$1
+
 if [ ! -e ~/.vim/bundle/Vundle.vim ]
 then
     echo "Installing Vundle"
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
 
-if [ ! -e ~/.vimrc ]
+if [ ! -e "~/.vimrc" ] || [ "$force" ]
 then
     echo "Copying vimrc to ~/.vimrc"
     cp .vimrc ~/.vimrc
 fi
 
-if [ ! -e ~/.tmux.conf ]
+if [ ! -e ~/.tmux.conf ] || [ "$force" ]
 then
     echo "Copying tmux.conf to ~/.tmux.conf"
     cp .tmux.conf ~/.tmux.conf
