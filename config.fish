8d69d388bac838b34e1dc60f881cedc79b1fdf13
Pär Berge <par.berge@tele2.com>
2018-02-01 10:41:39 +0100

    Add install fundle to config.fish

--- config.fish
+++ config.fish
@@ -1,3 +1,6 @@
+if not functions -q fundle; eval (curl -sfL https://git.io/fundle-install); end
+fundle plugin 'tuvistavie/fish-ssh-agent'
+
 function fish_prompt --description 'Write out the prompt'
     # Just calculate these once, to save a few cycles when displaying the prompt
     if not set -q __fish_prompt_hostname
